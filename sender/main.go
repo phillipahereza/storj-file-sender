cc07fe095474c66db81619cec88deee1ebe422c6
Samuel Hawksby-Robinson <samuel@samyoul.com>
2019-12-11 14:11:00 +0000

    Added get args and arg validator to sender application

--- sender/main.go
+++ sender/main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"flag"
+
+	"os"
+)
+
 func main() {
 	// get arguments
+	host := *flag.String("host", "", "Mandatory - The host of the file relay")
+	fn := *flag.String("file", "", "Mandatory - The name of the file you wish to transfer")
+
+	flag.Parse()
+
+	validateFlags(host, fn)
 
 	// get file
 
@@ -16,4 +28,24 @@ func main() {
 	// wait for transfer to complete
 
 	// no errors exit
+}
+
+func validateFlags(host string, fn string) {
+	failed := false
+	msg := "Error - Mandatory flag missing "
+
+	if host == "" {
+		println(msg + "'host'")
+		failed = true
+	}
+
+	if fn == "" {
+		println(msg + "'file'")
+		failed = true
+	}
+
+	if failed {
+		print("For help using this command tool please enter 'sender -h'")
+		os.Exit(1)
+	}
 }
